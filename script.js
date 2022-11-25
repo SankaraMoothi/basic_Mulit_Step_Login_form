522b298c8f2dcdab393ad5d73b6d73fc3ce14b26
SankaraMoothi <sankaramoorthi99@gmail.com>
2022-11-25 09:50:02 +0530

    finalstep

--- script.js
+++ script.js
@@ -9,26 +9,22 @@ let Back2 = document.getElementById("Back2");
 
 let progress = document.getElementById("progress");
 
-Next1.onclick = function (event) {
-  event.preventDefault();
+Next1.onclick = function () {
   Form1.style.left = "-450px";
   Form2.style.left = "40px";
   progress.style.width = "240px";
 };
-Back1.onclick = function (event) {
-  event.preventDefault();
+Back1.onclick = function () {
   Form1.style.left = "40px";
   Form2.style.left = "450px";
   progress.style.width = "120px";
 };
-Next2.onclick = function (event) {
-  event.preventDefault();
+Next2.onclick = function () {
   Form2.style.left = "-450px";
   Form3.style.left = "40px";
   progress.style.width = "360px";
 };
-Back2.onclick = function (event) {
-  event.preventDefault();
+Back2.onclick = function () {
   Form2.style.left = "40px";
   Form3.style.left = "450px";
   progress.style.width = "240px";
